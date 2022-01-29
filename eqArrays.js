e060839d7ed7b3775f1913fe52754bead60b21bf
Karolina Redden <dubajkaro@gmail.com>
2022-01-29 16:34:25 +0000

    adding comments to the code

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+//Original function which was asked to be copied from a diffrent task:
+
+const assertEqual = function(actual, expected) { //actual: true or false from eqArrays function and expected from the function call
   if (actual === expected) {
     console.log(` 👍 Assertion Passed: ${actual} === ${expected} `);
   } else {
@@ -7,25 +8,18 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE from the origal task (not relevant for this exc.)
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
-
-
 //Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match. 
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for ( i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
+const eqArrays = function(arr1, arr2) { //function takes two arrays
+  if (arr1.length === arr2.length) { // checking if arrays are the same length
+    for ( i = 0; i < arr1.length; i++) { //runs a loop on the first array
+      if (arr1[i] !== arr2[i]) { //checks if arguments are not the same
         return false;
       }
     }
     return true;
   }
 }
-
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
