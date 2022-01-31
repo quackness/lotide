15726bd92b077f85bb79666e5a4b40d04fdc9e21
Karolina Redden <dubajkaro@gmail.com>
2022-01-31 17:14:57 +0000

    linted

--- middle.js
+++ middle.js
@@ -28,24 +28,24 @@ const middle = function(arr) {
   }
   if (arr.length % 2 === 0) { ///the length of the array is an even number
     return [arr[arr.length / 2 - 1], arr[arr.length / 2 ]];
-  };
-}
+  }
+};
   
-// function should return the middle-most element 
-//for array with one or two values it should return an empty array 
+// function should return the middle-most element
+//for array with one or two values it should return an empty array
 //odd amount of numbers in the array should return one element > modulo
-//even amount of numbers should return 2 > modulo 
+//even amount of numbers should return 2 > modulo
 //write test assertions
 
-console.log(middle([1]))
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 11, 7, 6, 19, 33, 20])) // [ 11, 7 ]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+console.log(middle([1]));
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 11, 7, 6, 19, 33, 20])); // [ 11, 7 ]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
-//TESTS 
+//TESTS
 assertArraysEqual([1, 2], [ 1, 2], true);
 assertArraysEqual([1, 2], [], false);
 assertArraysEqual([1, 2, 3, 4, 11, 7, 6, 19, 33, 20], [1, 2, 3], false);
