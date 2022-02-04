d62d39c71eac81774be63ad56166bd8bf66a078a
Karolina Redden <dubajkaro@gmail.com>
2022-02-04 01:59:06 +0000

    map function with callback

---
+++ map.js
@@ -0,0 +1,43 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = []; //array bucket
+  for (let item of array) { //loop to go through each item in the array
+    results.push(callback(item)); //create an array with 1st letter of each item
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]); //callback details
+console.log(results1);
+
+
+//Our map function will take in two arguments:
+// An array to map
+//A callback function
+//The map function will return a new array based on the results of the callback function.
+
+//Code testing
+
+const assertArraysEqual = function(array1, array2, expected) {
+  if (eqArrays(array1, array2) === expected) {
+    console.log(` 👍 Assertion Passed: [${array1}] [${array2}] === ${expected} `);
+  } else {
+    console.log(` 👎 Assertion Failed: [${array1}] [${array2}] !== ${expected} `);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+assertArraysEqual([ 'g', 'c', 't', 'm', 't' ], [ 'g', 'c', 't', 'm', 't' ], true);
+assertArraysEqual([ 'g', 'c', 't', 'm', 't' ], [ 'o', 'o', 'o', 'p', 'i' ], false);
+assertArraysEqual([ 'g', 'i', 'm', 'a' ], [ 'o', 'o', 'o', 'p', 'i' ], false);
