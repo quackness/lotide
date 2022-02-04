bffe151825bcfef0321a18f4393a15e9dfbb1901
Karolina Redden <dubajkaro@gmail.com>
2022-02-04 20:28:54 +0000

    find key in object that matches callback condition

---
+++ findKey.js
@@ -0,0 +1,47 @@
+
+// const findKey = (object, callback) => {
+//   let items = Object.entries(object); //[["Blue Hill", { stars: 1 }]]
+//   for (let [key,value] of items) {
+//     if (callback(value) === true) {
+//       return key
+//     }
+//   }
+// };
+
+
+const findKey = (object, callback) => {
+  let items = Object.entries(object); //[["Blue Hill", { stars: 1 }]]
+  for (let item of items) {
+    if (callback(item[1]) === true) {
+      return item[0];
+    }
+  }
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+//go through the object, scan through names and stars
+//if starr === 2 then return the key
+//use object.entries it gives an array of the key at 0 and value at 1
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` 👍 Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(` 👎 Assertion Failed: ${actual} !== ${expected} `);
+  }
+};
+
+assertEqual("noma", "noma", true);
+assertEqual("noma", "Ora", false);
+assertEqual(" ", "Ora", false);
+assertEqual(" ", " ", true);
