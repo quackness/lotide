b7583271b9bcafd2a8a67f7cdb62aa1a9182cb68
Karolina Redden <dubajkaro@gmail.com>
2022-02-15 20:33:03 +0000

    incorporated mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,22 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;//assertion library by Chai
+const head = require('../head');//access to head.js
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5); //function head returns 5, assertEqual verifies if 5 === 5
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Hello === Hello
-assertEqual(head([1]),); // 1 !== undefined
-assertEqual(head([1]), 1); // 1 === 1
-assertEqual(head([]),); // undefined === undefined
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  it("returns undefined when array is empty", () => {
+    assert.isUndefined(head([]));//specifically checking for undefined
+  });
+});
+
+//5 passing tests
