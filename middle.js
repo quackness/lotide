c1a98e39911532408db83f153a5dcd5b35827d65
Karolina Redden <dubajkaro@gmail.com>
2022-02-15 14:41:47 +0000

    code refactoring exporting function and tests

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {// checking if arrays are the same length
-    for (let i = 0; i < arr1.length; i++) {//runs a loop on the first array
-      if (arr1[i] !== arr2[i]) {//checks if arguments are not the same
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;//arrays don't match length
-};
-
-
-const assertArraysEqual = function(array1, array2, expected) {
-  if (eqArrays(array1, array2) === expected) {
-    console.log(` 👍 Assertion Passed: [${array1}] [${array2}] === ${expected} `);
-  } else {
-    console.log(` 👎 Assertion Failed: [${array1}] [${array2}] !== ${expected} `);
-  }
-};
-
-
 const middle = function(arr) {
   if (arr.length <= 2) { // 2 or less arguments in the array, the most specific condition at the top
     return [];
@@ -32,22 +10,22 @@ const middle = function(arr) {
   }
 };
   
-// function should return the middle-most element
+module.exports = middle;
+
+//============
+// function *middle* should return the middle-most element
 //for array with one or two values it should return an empty array
 //odd amount of numbers in the array should return one element > modulo
 //even amount of numbers should return 2 > modulo
 //write test assertions
+// console.log(middle([1]));
+// console.log(middle([1, 2])); // => []
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 11, 7, 6, 19, 33, 20])); // [ 11, 7 ]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
-console.log(middle([1]));
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 11, 7, 6, 19, 33, 20])); // [ 11, 7 ]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+//TESTS have been moved to middleTest.js
 
-//TESTS
-assertArraysEqual([1, 2], [ 1, 2], true);
-assertArraysEqual([1, 2], [], false);
-assertArraysEqual([1, 2, 3, 4, 11, 7, 6, 19, 33, 20], [1, 2, 3], false);
-assertArraysEqual([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], true);
+
