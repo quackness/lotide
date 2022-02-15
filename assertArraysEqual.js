0076ec43ac3c10a1a97b8c5a8517f99cfc5a7809
Karolina Redden <dubajkaro@gmail.com>
2022-02-15 02:01:05 +0000

    bug fix in eqArrays added a return for when the arrays do not match length

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console. 
 const assertArraysEqual = function(array1, array2, expected) {
   if (eqArrays(array1, array2) === expected) {
     console.log(` 👍 Assertion Passed: [${array1}] [${array2}] === ${expected} `);
@@ -7,21 +7,23 @@ const assertArraysEqual = function(array1, array2, expected) {
   }
 };
 
-// Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console. 
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
-    for ( i = 0; i < arr1.length; i++) {
+    for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
-        return false;
+        return false;//if the arrays are not identical
       }
     }
-    return true;
+    return true;//arrays are identical
   }
-}
+  return false;//arrays don't match length
+};
 
 assertArraysEqual([1, 2, 3], [1, 2, 3], true);
 assertArraysEqual([1, 2, 3], [3, 2, 1], false);
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"], true);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3], false);
+assertArraysEqual(["1", "2", "3"], ["1", 2, 3], false);
+assertArraysEqual([], ["1", "2", "6"], false);
+assertArraysEqual(["dog", "cat"], ["1", "2", "6"], false);
+assertArraysEqual(["dog", "cat"], ["1", "2", "6"], true);
 
